a1adf85c7d2e78775a47069886a5422b833164b5
Tyler Napier <tylersnapier@gmail.com>
2024-05-13 21:59:14 -0400

    Created Scripts folder and added Speed variable to Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    //public or private reference
+    //data type (int, float, bool, string)
+    //every variable has a name
+    //optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
         
     }
 }
