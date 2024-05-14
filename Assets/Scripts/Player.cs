d7feffa9350390a75f5199aa7c8dc5b1c03d8c38
Alaric Alabaster <tylersnapier@gmail.com>
2024-05-14 08:06:12 -0400

    Added Player movement and movement boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    //public or private reference
-    //data type (int, float, bool, string)
-    //every variable has a name
-    //optional value assigned
+    
     [SerializeField]
     private float _speed = 3.5f;
-    // Start is called before the first frame update
+   
     void Start()
     {
-        //take the current position = new position (0, 0, 0)
+       
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
         
+        //if player position on the Y is greater than 0
+        //Y position 0
+        //else if position on the Y is less than -3.8f
+        //Y pos = -3.8f
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        //if Player on the X > 11
+        //X pos = -11
+        //else if Player on the X > -11
+        //X pos = 11
+
+        if (transform.position.x >= 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+
     }
 }
