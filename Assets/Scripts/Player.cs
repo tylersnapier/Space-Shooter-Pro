295ac7ec57cc332a4469beb6913a17b0ff8f1172
Alaric Alabaster <tylersnapier@gmail.com>
2024-05-15 07:40:00 -0400

    Cleaned up code and added laser prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     
     [SerializeField]
     private float _speed = 3.5f;
+    
+    [SerializeField]
+    private GameObject _laser;
    
     void Start()
     {
@@ -18,18 +21,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+        //if I hit the Space key
+        //Spawn GameObject
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laser, transform.position, Quaternion.identity);
+        }
+    }
+
+    void CalculateMovement()
+    {
+
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
-        
-        //if player position on the Y is greater than 0
-        //Y position 0
-        //else if position on the Y is less than -3.8f
-        //Y pos = -3.8f
 
         if (transform.position.y >= 0)
         {
@@ -41,11 +51,7 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, -3.8f, 0);
         }
 
-        //if Player on the X > 11
-        //X pos = -11
-        //else if Player on the X > -11
-        //X pos = 11
-
+        
         if (transform.position.x >= 11)
         {
             transform.position = new Vector3(-11, transform.position.y, 0);
@@ -54,6 +60,5 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11, transform.position.y, 0);
         }
-
     }
 }
