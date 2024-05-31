bb2214bfa860507812add42664ff1304093207f1
Alaric Alabaster <tylersnapier@gmail.com>
2024-05-31 12:31:10 -0400

    Spawn manager stops spawning on Player death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,11 +16,19 @@ public class Player : MonoBehaviour
     private float _canFire = -1f;
     [SerializeField]
     private int _lives = 3;
+
+    private SpawnManager _spawnManager;
    
     void Start()
     {
        
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager != null )
+        {
+            Debug.LogError("The SpawnManager is NULL.");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+        }
     }
 
     // Update is called once per frame
@@ -81,6 +89,8 @@ public class Player : MonoBehaviour
 
         if (_lives < 1)
         {
+            _spawnManager.OnPlayerDeath();
+
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,10 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    private bool _stopSpawning = false;
     void Start()
     {
         StartCoroutine(SpawnRoutine());
@@ -23,14 +27,20 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnRoutine()
     {
-        while (true)
+        while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+           GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
         }
         //while loop (infinite loop)
         //Instantiate enemy prefab
         //yield wait for 5 seconds
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
     }
 }
