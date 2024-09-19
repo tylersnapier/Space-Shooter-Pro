8287f39c6645048e427f557b06d580c96a0c1518
Alaric Alabaster <tylersnapier@gmail.com>
2024-09-18 21:02:57 -0400

    Began adding lives

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,15 +8,23 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text _scoreText;
-    
+    [SerializeField]
+    private Sprite[] _livesSprites;
+    [SerializeField]
+    private Image _LivesImg;
     void Start()
     {
-        //assign text component to handle
+        
         _scoreText.text = "Score:" + 0;
     }
 
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore;
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+
     }
 }
