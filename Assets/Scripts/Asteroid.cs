5d37841aa203560c9e819b032ccd2e90c26f9676
TylerSNapier <109185047+tylersnapier@users.noreply.github.com>
2024-09-23 08:09:54 -0400

    VFX Added
    Added asteroid, asteroid movement, and explosion animation

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+    [SerializeField]
+    private GameObject _explosionPrefab;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    //check for Laser collission (Trigger)
+    //instantiate explosion at the position of the asteroid 
+    //destroy the explosion after 3 seconds
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.tag == "Laser")
+      {
+        Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+      }
     }
 }
