fa562062bf8a52525b8787bc5fb25f4ffdf1aff8
TylerSNapier <109185047+tylersnapier@users.noreply.github.com>
2024-09-25 06:36:38 -0400

    General Commit
    Player can now quit application by pressing Escape key.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,13 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene(1);//Current Game Scene
         }
 
+        //if the Escape key is pressed
+        //Quit the application
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
     public void GameOver()
     {
